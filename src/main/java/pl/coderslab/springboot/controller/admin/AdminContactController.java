a091d2f01f5b1c463ce90256f7eeade01d986ae6
JoannaJakub <joannajakubczyk@interia.pl>
2021-07-05 16:14:54 +0200

    admin edit controller

--- src/main/java/pl/coderslab/springboot/controller/admin/AdminContactController.java
+++ src/main/java/pl/coderslab/springboot/controller/admin/AdminContactController.java
@@ -4,11 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.springboot.model.Contact;
-import pl.coderslab.springboot.model.Storage;
 import pl.coderslab.springboot.repository.ContactRepository;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,6 +36,28 @@ public class AdminContactController {
     public String adminContactDelete(@PathVariable long id) {
         contactRepository.deleteById(id);
         return "redirect:/adminContact";
+    }
+    @GetMapping(value = {"/adminContactEdit/{id}"})
+    public String adminContactEditForm(@PathVariable long id, Model model) {
+        model.addAttribute("adminContactEdit", contactRepository.findById(id));
+        return "admin/storage/adminContactEdit";
+    }
+
+    @PostMapping(value = {"adminContactEdit/{id}"})
+    public String adminContactEditSave(@Valid Contact contact) {
+        contactRepository.save(contact);
+        return "redirect:/adminContactConfirmEditing/{id}";
+    }
+
+    @RequestMapping("/adminContactConfirmEditing/{id}")
+    public String adminContactConfirmEditing(@PathVariable long id, Model model) {
+        Optional<Contact> contact = contactRepository.findById(id);
+        if (contact.isPresent()) {
+            model.addAttribute("adminContactConfirmEdit", contact.get());
+        } else {
+            return "admin/adminError";
+        }
+        return "admin/contact/adminContactConfirmEdit";
     }
 }
 
