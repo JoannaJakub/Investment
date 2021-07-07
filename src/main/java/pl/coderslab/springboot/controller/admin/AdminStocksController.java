cb5650a9148afa45a65488763a0fc34947364ae0
JoannaJakub <joannajakubczyk@interia.pl>
2021-07-07 20:43:16 +0200

    admin delete stocks controller

--- src/main/java/pl/coderslab/springboot/controller/admin/AdminStocksController.java
+++ src/main/java/pl/coderslab/springboot/controller/admin/AdminStocksController.java
@@ -5,13 +5,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.springboot.model.Stocks;
 import pl.coderslab.springboot.model.Storage;
 import pl.coderslab.springboot.repository.StocksRepository;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class AdminStocksController {
@@ -41,5 +44,27 @@ public class AdminStocksController {
             stocksRepository.save(stocks);
             return "admin/stocks/stocksSuccess";
         }
+    }
+
+    @RequestMapping("/stocksConfirmDelete")
+    public String stocksConfirmDelete() {
+        return "admin/stocks/stocksConfirmDelete";
+    }
+
+    @GetMapping(value = {"/stocksDelete/{id}"})
+    public String stocksDelete(@PathVariable long id) {
+        stocksRepository.deleteById(id);
+        return "redirect:/adminStocks";
+    }
+
+    @GetMapping(value = {"/stocksDetails/{id}"})
+    public String stocksDetails(@PathVariable long id, Model model) {
+        Optional<Stocks> stocksDetails = stocksRepository.findById(id);
+        if (stocksDetails.isPresent()) {
+            model.addAttribute("stocksDetails", stocksDetails.get());
+        }
+        else{ return "admin/adminError";
+        }
+        return "admin/stocks/stocksDetails";
     }
 }
